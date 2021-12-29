6b866498de7f1b28e7062dfb8e7fe825fea61b1c
Joseph Hunter <jrhunt1706@gmail.com>
2021-12-28 20:49:16 -0600

    updated

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import { Header, Nav, Main, Footer } from "./components";
 import * as state from "./store";
-
-// Add Axios
 import axios from "axios";
 
 import Navigo from "navigo";
@@ -12,6 +10,8 @@ dotenv.config();
 
 const router = new Navigo(window.location.origin);
 
+//Move router to the end AND add Router.Hooks
+
 function render(st) {
   document.querySelector("#root").innerHTML = `
     ${Header(st)}
@@ -20,10 +20,13 @@ function render(st) {
     ${Footer()}
   `;
   router.updatePageLinks();
+
   addEventListeners(st);
 }
 
+//  FUNCTION FOR EVENT LISTENERS
 function addEventListeners(st) {
+  // add event listeners to Nav items for navigation
   document.querySelectorAll("nav a").forEach(navLink =>
     navLink.addEventListener("click", event => {
       event.preventDefault();
@@ -37,8 +40,47 @@ function addEventListeners(st) {
     .addEventListener("click", () =>
       document.querySelector("nav > ul").classList.toggle("hidden--mobile")
     );
+
+  if (st.view === "Order") {
+    document.querySelector("form").addEventListener("submit", event => {
+      event.preventDefault();
+
+      const inputList = event.target.elements;
+      console.log("Input Element List", inputList);
+
+      const toppings = [];
+      // Interate over the toppings input group elements
+      for (let input of inputList.toppings) {
+        // If the value of the checked attribute is true then add the value to the toppings array
+        if (input.checked) {
+          toppings.push(input.value);
+        }
+      }
+
+      const requestData = {
+        customer: inputList.customer.value,
+        crust: inputList.crust.value,
+        cheese: inputList.cheese.value,
+        sauce: inputList.sauce.value,
+        toppings: toppings
+      };
+      console.log("request Body", requestData);
+
+      axios
+        .post(`${process.env.PIZZA_PLACE_API_URL}`, requestData)
+        .then(response => {
+          // Push the new pizza onto the Pizza state pizzas attribute, so it can be displayed in the pizza list
+          state.Pizza.pizzas.push(response.data);
+          router.navigate("/Pizza");
+        })
+        .catch(error => {
+          console.log("It puked", error);
+        });
+    });
+  }
 }
 
+//  ADD ROUTER HOOKS HERE ...
 router.hooks({
   before: (done, params) => {
     const page =
@@ -76,12 +118,10 @@ router.hooks({
   }
 });
 
+//ADD ROUTER HERE ...
 router
   .on({
     "/": () => render(state.Home),
-    ":page": params => {
-      let page = capitalize(params.page);
-      render(state[page]);
-    }
+    ":page": params => render(state[capitalize(params.page)])
   })
   .resolve();
